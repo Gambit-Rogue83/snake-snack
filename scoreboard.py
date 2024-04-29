d8f329d88d37385198f9e780b6aa86b8352ae8df
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2024-04-29 15:49:19 -0600

    High Score "Memory" data

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as hi:
+            self.high_score = int(hi.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -23,6 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as hi:
+                hi.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
 
